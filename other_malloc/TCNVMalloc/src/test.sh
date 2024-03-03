72d6cc0cb92c5b3ef16e3eaab33326310421e318
XjhUUp <1244840409@qq.com>
2024-03-03 13:11:48 +0800

    update

--- other_malloc/TCNVMalloc/src/test.sh
+++
@@ -1,6 +0,0 @@
-gcc -o tcnvmalloc_test pq.c tcnvmalloc.c test-bench.c -lpthread
-g++ -o addr_handle addr_handle.cpp
-
-../../../tools/pin-3.27-98718-gbeaa5d51e-gcc-linux/pin -t ../../../tools/pin-3.27-98718-gbeaa5d51e-gcc-linux/source/tools/ManualExamples/obj-intel64/pinatrace.so --  ./tcnvmalloc_test | ./addr_handle >> tcnvmalloc.txt
-
-python3 static_analy.py
--- tools/pin-3.27-98718-gbeaa5d51e-gcc-linux/test2.c
+++
@@ -1,8 +0,0 @@
-#include <math.h>
-
-int main(){
-    double a =2;
-    int ans=log2(a);
-    printf("%d\n",ans);
-    return 0;
-}
